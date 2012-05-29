c7bb4ada0a6cbaf944dde9efd946e52a01845ec1
matthiaskrgr <matthias.krueger@famsik.de>
2012-05-29 13:39:10 +0200

    autogen.sh:  Implement autogen.sh script which runs autoreconf -f -i git submodule init git dubmodule update so we don't need to run them one by one manually.

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+if (autoreconf -f -i) ; then
+	echo "autoreconf ran successfully."
+	echo "Initializing submodules..."
+	if (git submodule init) ; then
+		echo "Updating submodules..."
+		if (git submodule update) ; then
+			echo "Run './configure && make' to continue."
+		else
+			echo "Error: could not update submodules!"
+		fi
+	else
+		echo "Error: could not initialize submodules!"
+	fi
+else
+	echo "Running autoreconf failed, please make sure you have autoconf installed."
+fi
