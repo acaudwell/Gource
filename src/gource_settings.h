9782a29ff93771bbe758175e1086264c6b8d20b4
Ricardo Rodrigues Lucca <rlucca@gmail.com>
2016-02-02 22:20:11 -0200

    Adding shown file filters

--- src/gource_settings.h
+++ src/gource_settings.h
@@ -130,6 +130,7 @@ public:
     std::vector<std::string> highlight_users;
     std::vector<std::string> follow_users;
     std::vector<Regex*> hidden_file_filters;
+    std::vector<Regex*> shown_file_filters;
     std::vector<Regex*> user_filters;
     bool file_extensions;
 
@@ -144,7 +145,7 @@ public:
     TextureResource* file_graphic;
 
     int log_level;
-    
+
     GourceSettings();
 
     void setGourceDefaults();
