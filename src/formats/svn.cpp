474870a3d7af80e109e3fd5ab328039c259e6821
Andrew Caudwell <acaudwell@gmail.com>
2016-07-31 21:49:44 +1200

    Small tweak to avoid having 'Unknown' twice.
    New version number.

--- src/formats/svn.cpp
+++ src/formats/svn.cpp
@@ -245,9 +245,9 @@ bool SVNCommitLog::parseCommit(RCommit& commit) {
     TiXmlElement* authorE = leE->FirstChildElement("author");
 
     if(authorE != 0) {
-		// GetText() may return NULL, causing author instantiation to crash.
-        std::string author(authorE->GetText() ? authorE->GetText() : "Unknown");
-
+        // GetText() may return NULL, causing author instantiation to crash.
+        std::string author;
+        if(authorE->GetText()) author = authorE->GetText();
         if(author.empty()) author = "Unknown";
 
         commit.username = author;
